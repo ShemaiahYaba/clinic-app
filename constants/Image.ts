6b505a9ea4fa2722cdb183a7b372304a7031161e
ShemaiahYaba <67932039+ShemaiahYaba@users.noreply.github.com>
2025-06-17 11:07:29 +0100

    Project reset

--- constants/Image.ts
+++
@@ -1,9 +0,0 @@
-const SplashScreen = require("../assets/images/SplashScreen.jpg");
-const EmergencyOff = require("../assets/images/EmergencyOff.jpg");
-const EmergencyOn = require("../assets/images/EmergencyOn.png");
-const alertBell = require("../assets/images/notification_icon.png")
-
-
-export default {
-    SplashScreen, EmergencyOn,EmergencyOff, alertBell
-}
--- index.js
+++
@@ -1,9 +0,0 @@
-import { registerRootComponent } from "expo";
-
-import App from "./app";
-
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
-registerRootComponent(App);
-// If you want to use the app in a web environment, you can also import
